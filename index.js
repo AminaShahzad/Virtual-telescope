f85afd18893aa88c8b89d5442145bbd6dcad5287
AminaShahzad <128682413+AminaShahzad@users.noreply.github.com>
2025-03-06 08:58:52 +0500

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const { insertUser, getUsers, updateUser, deleteUser } = require("./server");
 
 // Insert a User
-insertUser({ focal_lenght: 250, tel_name: "SuperScope", aperture: 50.5 });
+insertUser({  tel_name: "SuperScope",focal_lenght: 250, aperture: 50.5 });
+//insertUser({  tel_name: "Meade 16\" LX200-ACF",focal_lenght: 4064, aperture: 406.4 });
+//insertUser({  tel_name: "Celestron 8\" EdgeHD 800 Schmidt-Cassegrain Telescope OTA",focal_lenght: 2032, aperture: 203.2 });
+//insertUser({  tel_name: "Celestron 14\" EdgeHD 1400 Flat Field Schmidt-Cassegrain Telescope OTA",focal_lenght: 3910, aperture: 356});
 
 // Retrieve Users
 getUsers();
 
 // Update a User (Replace `id_here` with a real MongoDB ObjectId)
-updateUser("67c7eb094ad3da09b4b68821", { focal_lenght: 30, aperture: 99 });
-
+updateUser("SuperScope", { focal_lenght: 30, aperture: 99 });
+getUsers();
 // Delete a User (Replace `id_here` with a real MongoDB ObjectId)
-deleteUser("67c7eb094ad3da09b4b68821");
+deleteUser("SuperScope");
